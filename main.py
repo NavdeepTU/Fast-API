19e3daba6d274eb3f9503633843c7e0c62941119
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-05-21 12:02:56 +0530

    path parameter anf HTTPException in fastapi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -22,9 +22,9 @@ def view():
     return data
 
 @app.get("/patient/{patient_id}") # endpoint to retrieve specific patient info
-def view_patient(patient_id: str): 
+def view_patient(patient_id: str = Path(..., description="ID of the patient in the database", example="P001")): 
     # load all the patients
     data = load_data()
     if patient_id in data:
         return data[patient_id]
-    return {"error": "patient not found"}
+    raise HTTPException(status_code=404, detail="Patient not found")
