46571d7b03694dcb91b035b2b029cb8ac7ae661b
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-06-09 05:52:17 +0530

    add response model and dockerfile

--- insurance_premium_prediction/app.py
+++ insurance_premium_prediction/app.py
@@ -4,39 +4,40 @@ from schema.user_input import UserInput
 from schema.prediction_response import PredictionResponse
 from model.predict import predict_output, model, MODEL_VERSION
 
-
 app = FastAPI()
 
-# human readable     
-@app.get("/")
+# human readable       
+@app.get('/')
 def home():
-    return {"message" : "Insurance Premium prediction API"}
+    return {'message':'Insurance Premium Prediction API'}
 
 # machine readable
-@app.get("/health")
+@app.get('/health')
 def health_check():
     return {
-        "status": "OK",
-        "version": MODEL_VERSION,
-        "model_loaded": model is not None
+        'status': 'OK',
+        'version': MODEL_VERSION,
+        'model_loaded': model is not None
     }
-        
-@app.post("/predict", response_model=PredictionResponse)
+
+@app.post('/predict', response_model=PredictionResponse)
 def predict_premium(data: UserInput):
 
     user_input = {
-        "bmi": data.bmi,
-        "age_group": data.age_group,
-        "lifestyle_risk": data.lifestyle_risk,
-        "city_tier": data.city_tier,
-        "income_lpa": data.income_lpa,
-        "occupation": data.occupation
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
     }
 
     try:
+
         prediction = predict_output(user_input)
 
-        return JSONResponse(status_code=200, content={"response": prediction})
+        return JSONResponse(status_code=200, content={'response': prediction})
+    
     except Exception as e:
 
         return JSONResponse(status_code=500, content=str(e))
--- insurance_premium_prediction/schema/user_input.py
+++ insurance_premium_prediction/schema/user_input.py
@@ -1,21 +1,21 @@
-from pydantic import BaseModel, Field, computed_field, field_validator 
-from typing import Annotated, Literal
+from pydantic import BaseModel, Field, computed_field, field_validator
+from typing import Literal, Annotated
 from config.city_tier import tier_1_cities, tier_2_cities
 
 
-# pydantic model to validate incoming data, provide metadata to client and add computed fields
+# pydantic model to validate incoming data
 class UserInput(BaseModel):
 
-    age: Annotated[int, Field(..., gt=0, lt=120, description="Age of the user")]
-    weight: Annotated[float, Field(..., gt=0, description="Weight of the user")]
-    height: Annotated[float, Field(..., gt=0, lt=2.5, description="Height of the user")]
-    income_lpa: Annotated[float, Field(..., gt=0, description="Annual salary of the user in LPA")]
-    smoker: Annotated[bool, Field(..., description="Is user a smoker")]
-    city: Annotated[str, Field(..., description="The city user belongs to.")]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
+    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
+    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary of the user in lpa')]
+    smoker: Annotated[bool, Field(..., description='Is user a smoker')]
+    city: Annotated[str, Field(..., description='The city that the user belongs to')]
     occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-       'business_owner', 'unemployed', 'private_job'], Field(..., description="Occupation of the user")]
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
     
-    @field_validator("city")
+    @field_validator('city')
     @classmethod
     def normalize_city(cls, v: str) -> str:
         v = v.strip().title()
@@ -24,7 +24,7 @@ class UserInput(BaseModel):
     @computed_field
     @property
     def bmi(self) -> float:
-        return self.weight / (self.height**2)
+        return self.weight/(self.height**2)
     
     @computed_field
     @property
